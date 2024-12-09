8db80eae79d32c7517996c9eb86993e60fa394e6
Leondro Lio <github@leondrolio.com>
2024-12-09 03:42:00 +0000

    use hash history

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Homepage from "./views/Homepage.vue";
 import PilkHack2025 from "./views/PilkHack2025.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [{ path: "/", component: Homepage }, { path: "/hack/2025", component: PilkHack2025 }],
 });
 
