29e5eaf72533fb4daceffa5fae08d8620b6757d8
Leondro Lio <github@leondrolio.com>
2024-12-10 03:27:50 +0000

    use vite-ssg to prerender cool pages

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { createRouter, createWebHashHistory } from "vue-router";
 import Homepage from "./views/Homepage.vue";
 import PilkHack2025 from "./views/PilkHack2025.vue";
+import { ViteSSG } from "vite-ssg";
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [{ path: "/", component: Homepage }, { path: "/hack/2025", component: PilkHack2025 }],
+export const createApp = ViteSSG(App, {
+  routes: [
+    { path: "/", component: Homepage },
+    { path: "/hack/2025", component: PilkHack2025 },
+  ],
 });
-
-createApp(App).use(router).mount("#app");
